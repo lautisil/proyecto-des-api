3d78148a7f22a3e73a58eaf4a770f44ccfaedf1d
Jorge Lautaro Silvestro <lautarosilvestro2000@hotmail.com>
2024-01-19 14:17:02 -0300

    Cambios en app y creacion de componentes

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Usuarios from './src/components/Usuarios';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola, Coder!</Text>
+      <Text>Bienvenidos a mi Aplicacion</Text>
+      <Usuarios/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/components/Usuarios.jsx
@@ -0,0 +1,14 @@
+import {Text, View } from 'react-native';
+
+function Usuarios(props) {
+    return (
+        <View>
+            <Text>Lista de Usuarios</Text>
+            <Text>Fernando</Text>
+            <Text>Miguel</Text>
+            <Text>Juan</Text>
+        </View>
+    )
+}
+
+export default Usuarios
