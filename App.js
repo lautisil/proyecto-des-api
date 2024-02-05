c4e981917c6261122a3c5e74a4b1e47beacf296c
Jorge Lautaro Silvestro <lautarosilvestro2000@hotmail.com>
2024-02-05 12:57:33 -0300

    agrego fotos, icono del carrito y flexbox

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Image , TextInput } from 'react-native';
 import Usuarios from './src/components/Usuarios';
+import iconcarrito from './assets/iconcarrito.png'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola, Coder!</Text>
-      <Text>Bienvenidos a mi Aplicacion</Text>
-      <Usuarios/>
-      <StatusBar style="auto" />
+
+      <View style={{flexDirection:'row', alignItems:'center', justifyContent:'center', gap:10}}>
+
+        <Image style={{width:40 , height:40}} source={iconcarrito}/>
+        <Text >C A R R I T O</Text>    
+
+      </View>
+
+      <TextInput 
+      style={{borderColor:"black", 
+        borderWidth: 1, 
+        borderRadius:10, 
+        width: '90%',
+        textAlign:'center',
+        paddingVertical:10,
+        backgroundColor:"gray"
+      }} 
+      placeholder='Ingrese un producto'/>
+
+      <Text>Remeras</Text>
+      <Text>Pantalones</Text>
+      <Text>Zapatillas</Text>
     </View>
   );
 }
@@ -21,3 +40,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+/* 
+<Text>Hola, Coder!</Text>
+<Text>Bienvenidos a mi Aplicacion</Text>
+<Usuarios/>
+<StatusBar style="auto" />
+
+IMAGEN CON URL
+<Image
+style={{ width:50 , height:50}}
+source={{uri: "https://t3.ftcdn.net/jpg/05/60/17/66/360_F_560176615_cUua21qgzxDiliiyiVGYjUnLSGnVLIi6.jpg"}} 
+/>
+ */
