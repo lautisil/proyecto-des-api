e68aebabad65b25f1fa8efdff4c7847a574704ec
Jorge Lautaro Silvestro <lautarosilvestro2000@hotmail.com>
2024-02-10 13:09:16 -0300

    creacion de Card.jsx

---
+++ src/components/Card.jsx
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const Card = ( {children} ) => {
+  return (
+    <View style={styles.container}>
+        {children}
+    </View>
+  )
+}
+
+export default Card
+
+const styles = StyleSheet.create({
+    container: {
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.23,
+        shadowRadius: 2.62,
+        elevation: 4,
+    },
+})
