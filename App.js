a627898fefe89d5a6fc2489f95bc15ce1607bcb3
Jorge Lautaro Silvestro <lautarosilvestro2000@hotmail.com>
2024-02-11 13:48:02 -0300

    carpeta fonts

--- App.js
+++ App.js
@@ -1,18 +1,14 @@
 import { useState } from 'react';
+import { useFonts } from 'expo-font';
 import Home from './src/screens/Home';
 import ItemListCategories from './src/screens/ItemListCategories';
-import { useFonts } from 'expo-font';
+import { fonts } from './src/global/fonts';
 
 export default function App() {
 
   const [categorySelected, setCategorySelected] = useState('')
 
-  const [fontsLoaded] = useFonts();
-
-  if (!fontsLoaded) {
-    return null
-  }
-
+  const [fontsLoaded] = useFonts(fonts);
 
   return (
     <>
@@ -88,119 +84,3 @@ export default function App() {
 
 });
 */
-
-/* 
-<Text>Hola, Coder!</Text>
-<Text>Bienvenidos a mi Aplicacion</Text>
-<Usuarios/>
-<StatusBar style="auto" />
-
-IMAGEN CON URL
-<Image
-style={{ width:50 , height:50}}
-source={{uri: "https://t3.ftcdn.net/jpg/05/60/17/66/360_F_560176615_cUua21qgzxDiliiyiVGYjUnLSGnVLIi6.jpg"}} 
-/>
-
-BOTONES
-TouchableOpacity (hace animacion de opacidad)
-Pressable (no hace animacion de opacidad)
- 
-
-
- const [inputValue, setInputValue] = useState('')
-  const [cartItems, setCartItems] = useState(DATA)
-  const [modalVisible, setModalVisible] = useState(false)
-  const [itemSelected, setItemSelected] = useState(null)
-
-
-  const handleInputChange = (value) => setInputValue
-  (value);
-
-  const handleModal = (id) => {
-    setModalVisible(true);
-    setItemSelected(id);
-  }
-
-
-  const addItem = () => {
-    const newItem = {
-      name: inputValue,
-      id: new Date().getTime()
-    }
-    setCartItems([...cartItems, newItem])
-  }
-
-  const removeItem = () => {
-    const filteredArray = cartItems.filter((item)=> item.id !== itemSelected)
-    setCartItems(filteredArray)
-    setModalVisible(false)
-  }
-
-  const RemoveModal = () => {
-    return (
-      <Modal animationType='slide' transparent visible={modalVisible}>
-        <View style={styles.modalContainer}>
-          <Text style={{fontSize: 20, fontWeight:'bold'}}>Quieres eliminar el producto?</Text>
-          <Pressable onPress={()=> setModalVisible(false)}>
-            <Text>NO</Text>
-          </Pressable>
-          <Pressable onPress={removeItem}>
-            <Text>SI</Text>
-          </Pressable>
-        </View>
-      </Modal>
-    );
-  };
-
-
-  return (
-    <View style={styles.container}>
-
-      <View style={styles.nav}>
-
-        <Image 
-          style={{width:40, 
-          height:40}} 
-          source={iconcarrito}
-        />
-        
-        <Text 
-          style={{fontSize:25}}>
-          C A R R I T O
-        </Text>    
-      </View>
-
-
-      <View style={{flexDirection:'row'}}>
-        <TextInput
-          onChangeText={handleInputChange}
-          value={inputValue}
-          style={styles.textinput}
-          placeholder='Ingrese un producto'
-        />
-        <Pressable onPress={addItem}>
-          <Text style={styles.boton}>
-            +
-          </Text>
-        </Pressable>
-      </View>
-
-      <RemoveModal />
-
-      <View style={styles.contcategorias}>
-        <FlatList 
-          data = {cartItems}
-          renderItem={({item}) => (
-            <View style={{width:400, flexDirection: 'row'}}>
-              <Text style={styles.categorias}>{item.name}</Text>
-              <Pressable onPress={() => handleModal(item.id)}>
-                <Text style={{fontSize:20, paddingLeft:20}}>Borrar</Text>
-              </Pressable>
-            </View>
-            )}
-            keyExtractor={(item) => item.id}
-          /> 
-      </View>
-
-    </View>
-  );*/
