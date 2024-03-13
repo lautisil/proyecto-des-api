35d6b3a6bb58b681b290a2c0ac7f465cc7af1803
Jorge Lautaro Silvestro <lautarosilvestro2000@hotmail.com>
2024-03-13 11:44:39 -0300

    configurancdo index

--- src/services/shopService.js
+++ src/services/shopService.js
@@ -1,1 +1,18 @@
 import { base_url } from "../firebase/database";
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
+
+export const shopApi = createApi({
+    reducerPath: "shopApi" ,
+    baseQuery: fetchBaseQuery({ baseUrl: base_url}),
+    endpoints: (builder) => ({
+        getProductsByCategory: builder.query({
+            query: (category) => `products.json?orderBy="category"&equalTo="${category}"`
+        }),
+        getCategories: builder.query({
+            query: () => 'categories.json'
+        }),
+    }
+    )
+})
+
+export const { useGetProductsByCategoryQuery, useGetCategoriesQuery } = shopApi
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit"
 import counterReducer from "../features/counter/counterSlice";
 import shopReducer from '../features/shop/shopSlice'
+import { shopApi } from "../services/shopService";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 export default configureStore({
   reducer: {
     counterReducer,
-    shopReducer
-  }
+    shopReducer,
+    [shopApi.reducerPath] : shopApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(shopApi.middleware),
 })
+
+setupListeners(configureStore.dispatch)
