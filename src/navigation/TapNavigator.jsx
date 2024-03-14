80d306ccbdb7ea60f02988ce298ce323fb52064d
Jorge Lautaro Silvestro <lautarosilvestro2000@hotmail.com>
2024-03-14 11:51:44 -0300

    arreglos de bugs

--- src/navigation/TapNavigator.jsx
+++ src/navigation/TapNavigator.jsx
@@ -1,71 +1,92 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native'
 import ShopStack from '../navigation/ShopStack'
-import CartStack from './CartStack';
+import CartStack from './CartStack'
 import { StyleSheet, Text, View } from "react-native"
-import { colors } from '../global/colors';
-import { Feather, Entypo, FontAwesome } from '@expo/vector-icons';
-import OrdersStack from './OrderStack';
-
+import { colors } from '../global/colors'
+import { Entypo, AntDesign, FontAwesome, Ionicons } from "@expo/vector-icons"
+import OrdersStack from './OrderStack'
+import MyProfileStack from "./MyProfileStack"
 
 const TapNavigator = () => {
     const Tab = createBottomTabNavigator()
 
-    return (
-            <Tab.Navigator
-            screenOptions={{
-                headerShown: false,
-                tabBarShowLabel: false,
-                tabBarStyle: styles.tabBar
-            }}>
-                <Tab.Screen name="ShockStack" component={ShopStack
-                } options={{
-                    tabBarIcon: ({focused})=> {
-                    return (
-                        <View style={styles.tabContainer}>
-                          <Entypo name="shop" size={30} color={focused ? "black" : "grey"} />
-                          <Text style={{ color: focused ? "black" : "grey" }}>S H O P</Text>
-                        </View>
-                    )
-                }}}
+return (
+  <Tab.Navigator
+  screenOptions={{
+      headerShown: false,
+      tabBarShowLabel: false,
+      tabBarStyle: styles.tabBar
+  }}>
+      <Tab.Screen 
+        name="ShockStack" 
+        component={ShopStack} 
+        options={{
+          tabBarIcon: ({focused}) => {
+            return (
+              <View style={styles.tabContainer}>
+                <Entypo name="shop" size={30} color={focused ? "black" : "grey"} />
+                <Text style={{ color: focused ? "black" : "grey" }}>S H O P</Text>
+              </View>
+            )
+          }
+        }}
+      />
+      <Tab.Screen 
+      name="CarStack" 
+      component={CartStack} 
+      options={{
+          tabBarIcon: ({focused})=> {
+          return (
+              <View style={styles.tabContainer}>
+                  <Feather name="shopping-cart" size={28} color={focused ? 'black' : 'grey'} />
+                  <Text style={{color:
+                      focused ?
+                       'black' :
+                        'grey'
+                        }}>C A R T</Text>
+              </View>
+          )
+      }}}
+      />
+      <Tab.Screen
+        name="OrdersTab"
+        component={OrdersStack}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <View style={styles.tabContainer}>
+                <FontAwesome
+                  name="list-ul"
+                  size={30}
+                  color={focused ? "black" : "grey"}
                 />
-                <Tab.Screen 
-                name="CarStack" 
-                component={CartStack} 
-                options={{
-                    tabBarIcon: ({focused})=> {
-                    return (
-                        <View style={styles.tabContainer}>
-                            <Feather name="shopping-cart" size={28} color={focused ? 'black' : 'grey'} />
-                            <Text style={{color:
-                                focused ?
-                                 'black' :
-                                  'grey'
-                                  }}>C A R T</Text>
-                        </View>
-                    )
-                }}}
+                <Text style={{ color: focused ? "black" : "grey" }}>Orders</Text>
+              </View>
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="MyProfileStack"
+        component={MyProfileStack}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <View style={styles.tabContainer}>
+                <Ionicons
+                  name="person-circle-outline"
+                  size={30}
+                  color={focused ? "black" : "grey"}
                 />
-                <Tab.Screen
-                  name="OrdersTab"
-                  component={OrdersStack}
-                  options={{
-                    tabBarIcon: ({ focused }) => {
-                      return (
-                        <View style={styles.tabContainer}>
-                          <FontAwesome
-                            name="list-ul"
-                            size={30}
-                            color={focused ? "black" : "grey"}
-                          />
-                          <Text style={{ color: focused ? "black" : "grey" }}>Orders</Text>
-                        </View>
-                      );
-                    },
-                  }}
-                />
-            </Tab.Navigator>
-    )
+                <Text style={{ color: focused ? "black" : "grey" }}>My profile</Text>
+              </View>
+            );
+          },
+        }}
+      />
+  </Tab.Navigator>
+)
 }
 
 export default TapNavigator
